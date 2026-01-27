dd8ec8b9ddb8ba465216494b3e01dbfcb0c8d884
Pedro Durand <webpedrod@gmail.com>
2026-01-27 20:23:52 -0300

    feat: corrigir importação da entidade AbilityScore para o caminho correto

--- src/modules/admin/admin.service.ts
+++ src/modules/admin/admin.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
-import { AbilityScoreEntity } from '../shared/entities/ability-score.entity';
+import { AbilityScoreEntity } from '../shared/entities/ability-scores.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
